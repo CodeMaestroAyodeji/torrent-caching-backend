f9cc2849e13f715d6e33e4813382ad81866ce2e4
CodeMaestroAyodeji <ajuwon2024@gmail.com>
2024-12-25 00:54:59 +0530

    Adding my backend

--- utils/templates/downloadCompleteEmail.js
+++ utils/templates/downloadCompleteEmail.js
@@ -4,5 +4,5 @@ exports.downloadCompleteEmail = (name, fileName) => `
   <h1>Hello, ${name}!</h1>
   <p>Your file <strong>${fileName}</strong> has been successfully downloaded and cached.</p>
   <p>Log in to your account to download it:</p>
-  <a href="${process.env.FRONTEND_DEV_URL}/bt-vaults/user/login">Login Here</a>
+  <a href="${process.env.FRONTEND_DEV_URL}/login">Login Here</a>
 `;
