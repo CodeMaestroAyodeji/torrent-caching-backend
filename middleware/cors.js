ce88d7165d33c43f73c84039ecc58f5a56306a26
CodeMaestroAyodeji <ajuwon2024@gmail.com>
2024-12-29 03:45:01 +0530

    Updating CorsOptions

--- middleware/cors.js
+++ middleware/cors.js
@@ -1,13 +1,11 @@
 const cors = require('cors');
 const corsOptions = require('../config/corsOptions');
 
-// Create middleware function
 const corsMiddleware = cors(corsOptions);
 
-// Handle preflight requests
 const handlePreflight = (req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
   if (req.method === 'OPTIONS') {
-    // Apply CORS headers for preflight requests
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS, PATCH');
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
     return res.status(200).send();
