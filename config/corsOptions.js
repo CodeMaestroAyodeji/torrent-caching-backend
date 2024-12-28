b53893c74722ebdb262af3bd591827e2ce42ff42
CodeMaestroAyodeji <ajuwon2024@gmail.com>
2024-12-29 03:54:12 +0530

    Updating CorsOptions

--- config/corsOptions.js
+++ config/corsOptions.js
@@ -1,18 +1,5 @@
-const whitelist = require('./whitelist');
-
 const corsOptions = {
-  origin: (origin, callback) => {
-    // Allow requests with no origin (like mobile apps or curl requests)
-    if (!origin) {
-      return callback(null, true);
-    }
-    
-    if (allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error(`Origin ${origin} not allowed by CORS`));
-    }
-  },
+  origin: '*', // Allow all origins
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'PATCH'],
   allowedHeaders: ['Origin', 'X-Requested-With', 'Content-Type', 'Accept', 'Authorization'],
@@ -21,5 +8,3 @@ const corsOptions = {
   preflightContinue: false,
   optionsSuccessStatus: 200 // Some legacy browsers (IE11) choke on 204
 };
-
-module.exports = corsOptions;
--- middleware/cors.js
+++ middleware/cors.js
@@ -1,16 +1,10 @@
-const cors = require('cors');
-const corsOptions = require('../config/corsOptions');
-
-const corsMiddleware = cors(corsOptions);
-
 const handlePreflight = (req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  if (req.method === 'OPTIONS') {
-    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS, PATCH');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
-    return res.status(200).send();
-  }
-  next();
-};
-
-module.exports = { corsMiddleware, handlePreflight };
+    res.header('Access-Control-Allow-Origin', '*'); // Allow all origins
+    if (req.method === 'OPTIONS') {
+      res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS, PATCH');
+      res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+      return res.status(200).send();
+    }
+    next();
+  };
+  
